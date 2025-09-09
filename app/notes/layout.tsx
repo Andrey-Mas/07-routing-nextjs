986b9560630fc4f7e51a9a7b28fc00d4fb1047f7
Andrey-Mas <a.kruegger93@gmail.com>
2025-09-09 18:20:05 +0200

    2

--- app/notes/layout.tsx
+++ app/notes/layout.tsx
@@ -1,15 +1,8 @@
+// app/notes/layout.tsx
 export default function NotesRootLayout({
   children,
-  overlay,
 }: {
   children: React.ReactNode;
-  overlay: React.ReactNode;
 }) {
-  // ВАЖЛИВО: overlay має бути поза основним контентом
-  return (
-    <>
-      {children}
-      {overlay}
-    </>
-  );
+  return <>{children}</>;
 }
